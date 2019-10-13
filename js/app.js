f556aa06320f96530e1d218740c871add378048a
NickBulbeck <bulbecknick@gmail.com>
2019-10-13 09:43:12 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const square1 = document.getElementById("square1");
+const square2 = document.getElementById("square2");
+const square3 = document.getElementById("square3");
+const square4 = document.getElementById("square4");
+const square5 = document.getElementById("square5");
+const square6 = document.getElementById("square6");
+const square7 = document.getElementById("square7");
+const square8 = document.getElementById("square8");
+const square9 = document.getElementById("square9");
+
+const square = document.querySelectorAll();
+
+
+
+console.log("Working!");
